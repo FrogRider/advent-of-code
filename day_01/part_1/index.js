8b3c9eb18e97452639f2a396f932ab5b608e2960
FrogRider <jhonny.lbk@gmail.com>
2023-12-01 19:19:11 +0200

    add day 1

---
+++ day_01/part_1/index.js
@@ -0,0 +1,28 @@
+import fs from 'fs'
+
+const readFile = (path) => {
+	const content = fs.readFileSync(path, 'utf-8').split('\n')
+	return content
+}
+
+const getCalibrationCodes = (input) => {
+	const data = readFile(input)
+	const firstAndLastDigit = data.map((el) => {
+		const digits = el.match(/\d+/g).join('').split('')
+
+		return [digits.at(0), digits.at(-1)].join('')
+	})
+
+	return firstAndLastDigit
+}
+
+const getTotalCalibrationNumber = (numbers) => {
+	return numbers.reduce((acc, current) => {
+		return (acc += +current)
+	}, 0)
+}
+
+console.log(
+	getTotalCalibrationNumber(getCalibrationCodes('./sample-input.txt'))
+)
+console.log(getTotalCalibrationNumber(getCalibrationCodes('./full-input.txt')))
