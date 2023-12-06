c70ba5a650b2dfbaeed42561c81e229a467bc832
FrogRider <jhonny.lbk@gmail.com>
2023-12-06 21:08:09 +0200

    add much more efficient solution for the second part of the 6 day

--- day_06/part_2/index.js
+++ day_06/part_2/index.js
@@ -13,17 +13,20 @@ const transformData = () => {
 const getAmountOfWinningCombos = () => {
 	const record = transformData()
 
-	const allTimes = Array.from({ length: ++record.time })
-		.map((_, index) => index)
-		.reverse()
-	const distanceToBeat = record.distance
-	// find index of th first time setting that satisfies the condition
-	const indexOfFirstWinningVariant = allTimes.findIndex(
-		(el, index) => el * index > distanceToBeat
-	)
-	// therefore we got amount of dissatisfying times from one side
-	// amount of dissatisfying times from the other side is same
-	return allTimes.length - indexOfFirstWinningVariant * 2
+	const { time, distance } = record
+	let firstWinningIndex = 0
+
+	// find the index of the first time setting that satisfies the condition
+	for (let i = time; i > 1; --i) {
+		if (i * (time - i) >= distance) {
+			firstWinningIndex = i
+			break
+		}
+	}
+
+	// therefore we get an amount of dissatisfying times from one side
+	// amount of dissatisfying times from the other side is the same
+	return Math.abs(time - firstWinningIndex * 2 - 1)
 }
 
 const path = './full-input.txt'
